c5a418fdd3b187454982215d5c9ff5deec15f70d
Cosmin Ionut Sora <sora.cosmin.ionut@gmail.com>
2022-04-10 11:37:35 +0300

    Update

--- 02_joins.sql
+++ 02_joins.sql
@@ -1,71 +1,84 @@
--- inner query / outer query (joins by WHERE and joins by FROM)
+-----------------------------------------------------------
+-- Inner / outer queries (joins by WHERE and joins by FROM)
+-----------------------------------------------------------
 SELECT e.ENAME, e.JOB, e.SAL, d.LOC
 FROM EMP e, (select * from DEPT where LOC = 'DALLAS') d -- inner query
-WHERE e.DEPTNO = d.DEPTNO -- outer query
+WHERE e.DEPTNO = d.DEPTNO; -- outer query
 
--- inner joins
-SELECT * -- classic inner join
+-----------------------------------------------------------
+-- Inner joins --> classical and new syntax
+-----------------------------------------------------------
+-- Classic inner join
+SELECT * 
 FROM emp e, dept d 
-WHERE e.deptno = d.deptno
-
-SELECT * -- new inner join syntax - SQL standard not only Oracle
+WHERE e.deptno = d.deptno;
+-- New inner join syntax - SQL standard not only Oracle
+SELECT * 
 FROM emp e INNER JOIN dept d 
-ON e.deptno = d.deptno 
+ON e.deptno = d.deptno;
 
--- outer joins
--- right joins
+-----------------------------------------------------------
+-- Outer joins --> right and left joins
+-----------------------------------------------------------
+-- Right joins
 SELECT * FROM dept
 SELECT *
 FROM emp e RIGHT JOIN dept d 
-ON e.deptno = d.deptno
+ON e.deptno = d.deptno;
 
--- left joins
+-- Left joins
 SELECT *
 FROM emp e LEFT OUTER JOIN dept d 
-ON e.deptno = d.deptno
+ON e.deptno = d.deptno;
 
-SELECT * -- classic outer join
+-- Classic outer join
+SELECT * 
 FROM emp e, dept d 
-WHERE e.deptno(+) = d.deptno -- (+) = outer right join operator
+WHERE e.deptno(+) = d.deptno; -- (+) = outer right join operator
 
-SELECT * -- classic outer join
+SELECT *
 FROM emp e, dept d 
-WHERE e.deptno = d.deptno(+) -- (+) = outer right join operator / this operator only works in oracle SQL
+WHERE e.deptno = d.deptno(+); -- (+) = outer right join operator / this operator only works in oracle SQL
 
--- full outer joins
+-- Full outer joins
 SELECT * 
 FROM emp e FULL OUTER JOIN dept d 
-ON e.deptno = d.deptno
+ON e.deptno = d.deptno;
 
--- outer join using the subqueries / nested queries
+-- Outer join using the subqueries / nested queries
 SELECT *
 FROM (select * from emp where job = 'SALESMAN') e FULL OUTER JOIN (select * from dept) d 
-ON e.deptno = d.deptno
+ON e.deptno = d.deptno;
 
--- exercise
+-- Exercise
 SELECT e.empno as empno, e.ename as ename, e.job as job, e.mgr as mgr, e.hiredate as hiredate, e.sal as sal, e.comm as comm, e.deptno as deptno, d.deptno as deptno, d.dname as dname, d.loc as loc
 FROM (select * from dept) d LEFT OUTER JOIN (select * from emp where job = 'SALESMAN') e
-ON e.deptno = d.deptno
+ON e.deptno = d.deptno;
 
 SELECT e.*, d.*
 FROM (select * from dept) d LEFT OUTER JOIN (select * from emp where job = 'SALESMAN') e
-ON e.deptno = d.deptno
+ON e.deptno = d.deptno;
 
--- self join (where the same data exists in the same table but means different things)
+-----------------------------------------------------------------------------------------
+-- Self joins --> where the same data exists in the same table but means different things
+-----------------------------------------------------------------------------------------
 SELECT * FROM emp
 SELECT *
 FROM emp INNER JOIN emp
-ON EMPNO = MGR
+ON EMPNO = MGR;
 
 SELECT *
 FROM emp e, (select empno, ename from emp) d
-WHERE e.mgr = d.empno
+WHERE e.mgr = d.empno;
 
 SELECT e.empno, e.ename, e.mgr, d.empno, d.ename
 FROM emp e LEFT JOIN (select * from emp) d
-ON e.mgr = d.empno
+ON e.mgr = d.empno;
 
--- cartesian product / cross join / no join = un join intre doua tabele fara o conditie dupa care sa se faca join-ul (fiecare rand din emp cu fiecare rand din dept)
+-------------------------------------------------------------------------------------------------------------------------
+-- Cross joins (cartesion product, no join) 
+-- un join intre doua tabele fara o conditie dupa care sa se faca join-ul (fiecare rand din emp cu fiecare rand din dept)
+-------------------------------------------------------------------------------------------------------------------------
 SELECT * FROM emp, dept;
 
 SELECT *
@@ -75,8 +88,12 @@ ON 1=1;
 SELECT *
 FROM emp CROSS JOIN dept;
 
--- natural join = un join care nu are nevoie de conditie pentru ca ia automat daca primary key-ul unui tabel exista ca foreign key in cel de-al doilea tabel
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- Natural joins
+-- un join care nu are nevoie de conditie pentru ca ia automat daca primary key-ul unui tabel exista ca foreign key in cel de-al doilea tabel
+---------------------------------------------------------------------------------------------------------------------------------------------
 SELECT * FROM EMP;
+
 -- in acest caz a luat automat dupa deptno care e primary key in dept si foreign key in emp
 SELECT *
 FROM emp NATURAL JOIN dept;
@@ -90,7 +107,11 @@ SELECT *
 FROM emp JOIN dept
 USING (col1, col2); -- pentru cazul in care key-ul are nume diferita in tabela 1 fata de tabela 2
 
--- equijoin = un inner join al carei conditii este o egalitate
+----------------------------------------------------------------------------
+-- Equijoins -> un inner join al carei conditii este o egalitate
+-- Non-equijoins -> un join care are ca si conditie o relatie de inegalitate
+----------------------------------------------------------------------------
+-- Equijoin
 SELECT *
 FROM emp, dept
 WHERE emp.deptno = dept.deptno;
@@ -99,7 +120,7 @@ SELECT *
 FROM emp JOIN dept
 ON emp.deptno = dept.deptno;
 
--- non-equijoin = un join care are ca si conditie o relatie de inegalitate
+-- Non-equijoin
 CREATE TABLE job_grade
 (Grade_level varchar(2) not null,
 lowest_sal number not null,
--- 03_exists.sql
+++ 03_exists.sql
@@ -1,19 +1,20 @@
--- exists and not exists
+-- Exists
 SELECT *
 FROM emp e 
 WHERE EXISTS (select * from emp where job = 'SALESMAN')
 
+-- Not exists
 SELECT *
 FROM emp e 
 WHERE NOT EXISTS (select * from emp where job = 'SALESMAN')
 
 -- selecteaza doar ce exista in tabelul din where exists
+-- selecteaza departamentele unde exista un angajat mapat in tabela de angajati
 SELECT d.*
 FROM dept d 
 WHERE EXISTS (select * from emp where d.deptno = emp.deptno)
--- selecteaza departamentele unde exista un angajat mapat in tabela de angajati
 
+-- selecteaza departamentele unde nu exista un angajat mapat in tabela de angajati
 SELECT d.*
 FROM dept d 
 WHERE NOT EXISTS (select * from emp where d.deptno = emp.deptno)
--- selecteaza departamentele unde nu exista un angajat mapat in tabela de angajati
